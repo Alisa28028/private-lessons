d1064c472061063ab8c1b493ce7789cc8c02ec7d
Arisa Segawa <arisa.segawa@gmail.com>
2024-12-27 18:53:37 +0900

    wip

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,25 +9,44 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = if @event
+      @event.posts.build # A new post for this event
+    else
+      Post.new #Blank post
   end
 
   def create
-    @post = Post.new(post_params)
-    @post.user = current_user
+    @post = if @event
+      @event.posts.build(post_params)
+    else
+      Post.new(post_params)
+    end
 
     if @post.save
-      redirect_to @post, notice: 'Post successfully created.'
+      redirect_to event_post_path(@event, @post), notice: 'Post was successfully created.'
     else
-      render :new, status: :unprocessable_entity
+      render :new
     end
+  end
+
+    # @post = Post.new(post_params)
+    # @post.user = current_user
+
+    # if @post.save
+    #   redirect_to @post, notice: 'Post successfully created.'
+    # else
+    #   render :new, status: :unprocessable_entity
+    # end
   end
 
 
   def create_from_event
-    @event = Event.find(params[:id])
+    @event = Event.find(params[:event_id])
     @post = Post.new(post_params)
     @post.user = current_user
+
+    # Setting the title, description and video from the event
+    @post.title = @event.title
     @post.description = @event.description
     @post.videos.attach(@event.video) if @event.video.attached?
     if @post.save
