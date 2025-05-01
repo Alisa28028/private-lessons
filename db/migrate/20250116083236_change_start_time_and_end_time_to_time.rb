d98c01542433e497a3332efc048d0ed7b6f89655
Arisa Segawa <arisa.segawa@gmail.com>
2025-05-01 17:41:28 +0900

    updated migration

--- db/migrate/20250116083236_change_start_time_and_end_time_to_time.rb
+++ db/migrate/20250116083236_change_start_time_and_end_time_to_time.rb
@@ -1,8 +1,8 @@
 class ChangeStartTimeAndEndTimeToTime < ActiveRecord::Migration[7.1]
   def change
     # Remove the existing datetime columns
-    remove_column :events, :start_time, :datetime
-    remove_column :events, :end_time, :datetime
+    # remove_column :events, :start_time, :datetime
+    # remove_column :events, :end_time, :datetime
 
     # Add new columns with time type
     add_column :events, :start_time, :time
