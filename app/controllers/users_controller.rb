70d23bc3a02b1c19f756349d127a06e7f5ea137d
Arisa Segawa <arisa.segawa@gmail.com>
2025-06-17 10:48:07 +0900

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,7 +72,7 @@ class UsersController < ApplicationController
     .joins(:event_instance)
     .where(
       state: "unpaid",
-      status: ["confirmed", "cancelled"],
+      status: ["confirmed", "cancelled_by_teacher"],
       event_instances: { id: @event_instances.select(:id) }
     )
     # Attended students
