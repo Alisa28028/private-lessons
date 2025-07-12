d0a6fff7afe270eb152ae1931ff00f9c79b63f54
Arisa Segawa <arisa.segawa@gmail.com>
2025-07-12 23:50:01 +0900

    ?

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,19 +1,9 @@
-// Import Turbo (so Turbo frames and Turbo streams can work)
-import { Turbo } from "@hotwired/turbo-rails"
+// Import Turbo (no need to assign if unused)
+import "@hotwired/turbo-rails"
 
-// Import Stimulus (for managing front-end interactivity)
-import { Application } from "@hotwired/stimulus"
-const application = Application.start()
+// Import Stimulus application (starts Stimulus and registers all controllers)
+import "controllers"
 
-// Configure Stimulus development experience (optional for production)
-application.debug = false
-window.Stimulus = application
-
-// Export the Stimulus application for later use in your Stimulus controllers
-export { application }
-
-
+// Import ActionText and Trix
 import "trix"
 import "@rails/actiontext"
-import "./trix_underline_extension_controller"
-import "./trix_underline_controller"
