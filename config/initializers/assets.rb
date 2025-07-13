bff68eb6602d8ad7e3d6c7cbca459a2913dfaf9b
Arisa Segawa <arisa.segawa@gmail.com>
2025-07-13 14:28:37 +0900

    idk anymore

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,13 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
-# Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
 
-# Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
+# Add JS/CSS and other assets
+Rails.application.config.assets.precompile += %w(
+  bootstrap.min.js
+  popper.js
+  manifest.json
+)
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w(bootstrap.min.js popper.js manifest.json icons/*.png)
+# Precompile all PNGs inside app/assets/images/icons/
+Rails.application.config.assets.precompile += Dir.glob(
+  Rails.root.join("app/assets/images/icons/*")
+).map { |f| "icons/#{File.basename(f)}" }
