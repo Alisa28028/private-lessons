6abc25302ccae4fa0ce3429a0dc610d0cbb07ab9
Arisa Segawa <arisa.segawa@gmail.com>
2025-07-19 14:50:37 +0900

    fixed the description field youhouuu

--- config/initializers/action_text_sanitizer.rb
+++ config/initializers/action_text_sanitizer.rb
@@ -1,13 +1,16 @@
-Rails.application.config.to_prepare do
-  sanitizer = ActionText::ContentHelper.sanitizer
+Rails.application.config.after_initialize do
+  sanitizer_class = Rails::Html::SafeListSanitizer
 
-  class << sanitizer
-    def allowed_tags
-      super + Set.new(['u'])
+  sanitizer_class.class_eval do
+    alias_method :original_allowed_tags, :allowed_tags
+    alias_method :original_allowed_attributes, :allowed_attributes
+
+    def allowed_tags(*args, **kwargs)
+      original_allowed_tags(*args, **kwargs) + ['u']
     end
 
-    def allowed_attributes
-      super + Set.new(['style'])
+    def allowed_attributes(*args, **kwargs)
+      original_allowed_attributes(*args, **kwargs) + ['style']
     end
   end
 end
